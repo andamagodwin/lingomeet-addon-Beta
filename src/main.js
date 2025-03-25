57aa69636a079eef201b23cd2616164831597743
andamagodwin <andamagodwinezra@gmail.com>
2025-03-25 22:12:02 +0300

    module error attempt to fix

--- src/main.js
+++ src/main.js
@@ -1,28 +1,10 @@
-// Copyright 2024 Google LLC
-//
-// Licensed under the Apache License, Version 2.0 (the "License");
-// you may not use this file except in compliance with the License.
-// You may obtain a copy of the License at
-//
-//    http://www.apache.org/licenses/LICENSE-2.0
-//
-// Unless required by applicable law or agreed to in writing, software
-// distributed under the License is distributed on an "AS IS" BASIS,
-// WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
-// See the License for the specific language governing permissions and
-// limitations under the License.
-
 import { meet } from '@googleworkspace/meet-addons/meet.addons';
 
 const CLOUD_PROJECT_NUMBER = '738129672596';
 const MAIN_STAGE_URL =
   'https://andamagodwin.github.io/lingomeet-addon-Beta/src/MainStage.html';
 
-/**
- * Prepares the Add-on Side Panel Client, and adds an event to launch the
- * activity in the main stage when the main button is clicked.
- */
-export async function setUpAddon() {
+async function setUpAddon() {
   const session = await meet.addon.createAddonSession({
     cloudProjectNumber: CLOUD_PROJECT_NUMBER,
   });
@@ -34,13 +16,15 @@ export async function setUpAddon() {
     });
 }
 
-/**
- * Prepares the Add-on Main Stage Client, which signals that the add-on has
- * successfully launched in the main stage.
- */
-export async function initializeMainStage() {
+async function initializeMainStage() {
   const session = await meet.addon.createAddonSession({
     cloudProjectNumber: CLOUD_PROJECT_NUMBER,
   });
   await session.createMainStageClient();
 }
+
+// Explicitly attach functions to window object
+window.helloWorld = {
+  setUpAddon,
+  initializeMainStage
+};
